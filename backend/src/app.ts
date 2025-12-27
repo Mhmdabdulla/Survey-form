d600726611d9baf1d65ba1a949e71389aa464944
Mhmdabdulla <96muhammedabdulla@gmail.com>
2025-12-27 14:16:27 +0530

    feat: connect server to mongodb

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,12 @@
 import express from "express";
 import cors from "cors";
+import connectDB from "./config/db.js";
+import * as dotenv from "dotenv";
 
 const app = express();
+
+dotenv.config();
+connectDB();
 
 app.use(cors());
 app.use(express.json());
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,8 @@
 
 import app from "./app.js";
 
+let port = process.env.PORT || 5000;
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+app.listen(port, () => {
+  console.log("Server running on port",port);
 });
