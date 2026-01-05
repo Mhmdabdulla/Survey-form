0e6b5187ae0f854621051ab07dcb625801e8bf3a
Mohammed Abdulla C <116338289+Mhmdabdulla@users.noreply.github.com>
2026-01-05 16:07:37 +0530

    Admin session (#3)
    * feat: completed session handling in frontend
    
    * fix:fixed cors issue by adding deployed url to cors origin

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -14,7 +14,7 @@ connectDB();
 
 app.use(
     cors({
-    origin: "http://localhost:5173",
+    origin:[ "http://localhost:5173","https://survey-form-vert-eight.vercel.app/"],
     credentials: true
   })
 );
