6e1f2d279ddcfbfa25ddb8e3d34eab71a9084469
Mhmdabdulla <96muhammedabdulla@gmail.com>
2026-01-05 17:27:05 +0530

    fix: Implement constant axios api for survey apis

--- frontend/src/services/survey.service.js
+++ frontend/src/services/survey.service.js
@@ -1,34 +1,13 @@
-import axios from 'axios';
-
-
-
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api';
+import API from "../config/axios";
 
 export const submitSurvey = async (payload) => {
   try {
-    const response = await axios.post(
-      `${API_BASE_URL}/surveys`,
-      payload,
-      {
-        withCredentials: true,
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      }
-    );
+    const response = await API.post('/surveys', payload);
 
     return response.data;
   } catch (error) {
-    // Axios error handling
-    if (axios.isAxiosError(error)) {
-      const message =
-        error.response?.data?.message ||
-        error.response?.data ||
-        'Failed to submit survey';
-
-      throw new Error(message);
-    }
-
-    throw new Error('Something went wrong while submitting the survey');
+    console.error("Login error from service:", error);
+    const errorMessage = error.response?.data?.message || "Login failed. Please try again.";
+    throw new Error(errorMessage);
   }
 };
